9a72330c88a721d05ebff2d031a289eda360343c
Nohegeomic <nohemymorell@hotmail.com>
2024-09-03 00:07:08 -0600

    Ordenes de trabajos asignadas

--- src/components/OrdenesTrabajo.js
+++ src/components/OrdenesTrabajo.js
@@ -1,12 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import '../styles/ordenes.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faPenSquare, faTrashCan, faBackward, faTrash  } from '@fortawesome/free-solid-svg-icons';
+import { faPenSquare, faTrashCan, faBackward, faTrash, faCheck  } from '@fortawesome/free-solid-svg-icons';
 import {supabase}  from '../supabaseClient';
 
 function OrdenesTrabajo() {
-
+  const currentUser = JSON.parse(localStorage.getItem('loggedInUser'));
   const [ordenes, setOrdenes] = useState([]);
+  const [userOrders, setUserOrders] = useState([]);
 
   useEffect(() => {
     const fetchOrders = async () => {
@@ -15,13 +16,15 @@ function OrdenesTrabajo() {
         .select('*');
 
         if (error) {
-            console.error('Error fetching data:', error);
+          console.error('Error fetching data:', error);
         } else {
-            setOrdenes(data);
+          setOrdenes(data);
+          const filteredUserOrders = data.filter(order => order.asignado === currentUser.id);
+          setUserOrders(filteredUserOrders);
         }
     };
       fetchOrders();
-    }, []);
+    }, [currentUser]);
 
   const confirmarEliminar = (id) => {
     // e.preventDefault();
@@ -63,56 +66,139 @@ function OrdenesTrabajo() {
     }
   };
 
+  const editOrden = async (id) => {
+    const confirmation = window.confirm('¿Seguro de tomar esta orden de trabajo?');
+    if(confirmation){
+      const { error } = await supabase
+      .from('ordentrabajo')
+      .update({ Estado: 'En proceso', asignado: currentUser.id})
+      .eq('id', id)
+      .select()
+
+      if(error){
+        console.log(error)
+      }else{
+        const updatedOrdenes = ordenes.map(orden =>
+          orden.id === id ? { ...orden, Estado: 'En proceso' } : orden
+        );
+        setOrdenes(updatedOrdenes);
+      }
+    }
+  }
+
+  const terminar = async (id) => {
+    const confirmation = window.confirm('¿Seguro de finalizar orden de trabajo?');
+    if(confirmation){
+      const { error } = await supabase
+      .from('ordentrabajo')
+      .update({ Estado: 'Finalizado'})
+      .eq('id', id)
+      .select()
+
+      if(error){
+        console.log(error)
+      }else{
+        const updatedOrdenes = ordenes.map(orden =>
+          orden.id === id ? { ...orden, Estado: 'Finalizado' } : orden
+        );
+        const updatedUserOrdenes = userOrders.map(orden =>
+          orden.id === id ? { ...orden, Estado: 'Finalizado' } : orden
+        );
+        setOrdenes(updatedOrdenes);
+        setUserOrders(updatedUserOrdenes)
+      }
+    }
+  }
+
   return (
-   <div className="ordenesList">
-    <h1 id='tituloOrdenes'>Ordenes de Trabajo</h1>
-      <table className="user-table">
+    <div className="ordenesContainer">
+      <div className="ordenesList">
+        <h1 id='tituloOrdenes'>Todas las Ordenes de Trabajo</h1>
+        <table className="user-table">
           <thead>
-              <tr>
-                <th>ID orden</th>
-                <th>Cliente</th>
-                <th>Teléfono</th>
-                <th>Fecha</th>
-                <th>Estado</th>
-                <th>Servicios</th>
-                <th>Acciones</th>
-              </tr>
+            <tr>
+              <th>ID orden</th>
+              <th>Cliente</th>
+              <th>Teléfono</th>
+              <th>Fecha</th>
+              <th>Estado</th>
+              <th>Servicios</th>
+              <th>Acciones</th>
+            </tr>
           </thead>
           <tbody>
-              {ordenes.map((orden) => (
-                  <tr key={orden.id}>
-                    <td>{orden.id}</td>
-                    <td>{orden.cliente}</td>
-                    <td>{orden.telefono}</td>
-                    <td>{orden.fecha}</td>
-                    <td>Recibida</td>
-                    <td>{orden.servicios.join(', ')}</td>
-                    <td>
-                     <div className="acciones" id={'originalAcc'+orden.id}>
-                        <button className='editAction' onClick={() => {/* tu lógica aquí */}}>
-                        <FontAwesomeIcon icon={faPenSquare} />
-                        </button>
-                        <button className='deleteAction' onClick={() => confirmarEliminar(orden.id)}>
-                        <FontAwesomeIcon icon={faTrashCan} />
-                        </button>
-                    </div>
-                    <div className="hidden" id={'confElim'+orden.id}>
-                      <button className='backwards' onClick={() => desconfirmarEliminar(orden.id)}>
-                        <FontAwesomeIcon icon={faBackward} />
-                      </button>
-                      <button className='deleteAction' onClick={() => deleteOrden(orden.id)}>
-                        <FontAwesomeIcon icon={faTrash} />
-                      </button>
-                    </div>
-
-                    </td>
-                  </tr>
-              ))}
+            {ordenes.map((orden) => (
+              <tr key={orden.id}>
+                <td>{orden.id}</td>
+                <td>{orden.cliente}</td>
+                <td>{orden.telefono}</td>
+                <td>{orden.fecha}</td>
+                <td>{orden.Estado}</td>
+                <td>{orden.servicios.join(', ')}</td>
+                <td>
+                  <div className="acciones" id={'originalAcc'+orden.id}>
+                    <button className='editAction' onClick={() => editOrden(orden.id)}>
+                      <FontAwesomeIcon icon={faPenSquare} />
+                    </button>
+                    <button className='deleteAction' onClick={() => confirmarEliminar(orden.id)}>
+                      <FontAwesomeIcon icon={faTrashCan} />
+                    </button>
+                  </div>
+                  <div className="hidden" id={'confElim'+orden.id}>
+                    <button className='backwards' onClick={() => desconfirmarEliminar(orden.id)}>
+                      <FontAwesomeIcon icon={faBackward} />
+                    </button>
+                    <button className='deleteAction' onClick={() => deleteOrden(orden.id)}>
+                      <FontAwesomeIcon icon={faTrash} />
+                    </button>
+                  </div>
+                </td>
+              </tr>
+            ))}
           </tbody>
-      </table>
-   </div>
+        </table>
+      </div>
+
+      <div className="ordenesList">
+        <h2 id='tituloMisOrdenes'>Mis Ordenes de Trabajo</h2>
+        <table className="user-table">
+          <thead>
+            <tr>
+              <th>ID orden</th>
+              <th>Cliente</th>
+              <th>Teléfono</th>
+              <th>Fecha</th>
+              <th>Estado</th>
+              <th>Servicios</th>
+              <th>Acciones</th>
+            </tr>
+          </thead>
+          <tbody>
+            {userOrders.map((orden) => (
+              <tr key={orden.id}>
+                <td>{orden.id}</td>
+                <td>{orden.cliente}</td>
+                <td>{orden.telefono}</td>
+                <td>{orden.fecha}</td>
+                <td>{orden.Estado}</td>
+                <td>{orden.servicios.join(', ')}</td>
+                <td>
+                  <div className="acciones" id={'originalAcc'+orden.id}>
+                    <button className='terminadoAction' onClick={() => terminar(orden.id)}>
+                      <FontAwesomeIcon icon={faCheck} />
+                    </button>
+                    
+                  </div>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
   );
 }
+
 
 export default OrdenesTrabajo;
 
