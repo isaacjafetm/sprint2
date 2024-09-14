d9fc2f400179e3f147b33417da18744c20f83dba
memelopez <meme30lopez@gmail.com>
2024-09-14 10:26:43 -0600

    Arreglar errores de sparkling

--- src/components/Productos.js
+++ src/components/Productos.js
@@ -1,53 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import '../styles/productos.css';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {supabase}  from '../supabaseClient';
 
 function Productos () {
-  const prods = [
-    {
-      id: 1,
-      nombre: "termo Trek",
-      precio: 800,
-      cantidad: 30,
-      img: "https://p.vitalmtb.com/photos/features/2050/title_image/s1600_Leatt2018_BikeDBX_8901_ChrisLaue_656882.jpg?VersionId=_lWWPlR_wJqBMAFX4I1H6qhqT04cjStP"
-    },
-    {
-      id: 2,
-      nombre: "Casco",
-      precio: 2300,
-      cantidad: 24,
-      img: "https://p.vitalmtb.com/photos/features/2050/title_image/s1600_Leatt2018_BikeDBX_8901_ChrisLaue_656882.jpg?VersionId=_lWWPlR_wJqBMAFX4I1H6qhqT04cjStP"
-    },
-    {
-      id: 3,
-      nombre: "Rodilleras",
-      precio: 1200,
-      cantidad: 15,
-      img: "https://p.vitalmtb.com/photos/features/2050/title_image/s1600_Leatt2018_BikeDBX_8901_ChrisLaue_656882.jpg?VersionId=_lWWPlR_wJqBMAFX4I1H6qhqT04cjStP"
-    },
-    {
-      id: 4,
-      nombre: "Tenis 5",
-      precio: 1700,
-      cantidad: 9,
-      img: "https://p.vitalmtb.com/photos/features/2050/title_image/s1600_Leatt2018_BikeDBX_8901_ChrisLaue_656882.jpg?VersionId=_lWWPlR_wJqBMAFX4I1H6qhqT04cjStP"
-    },
-    {
-      id: 5,
-      nombre: "Guantes Trek",
-      precio: 500,
-      cantidad: 25,
-      img: "https://p.vitalmtb.com/photos/features/2050/title_image/s1600_Leatt2018_BikeDBX_8901_ChrisLaue_656882.jpg?VersionId=_lWWPlR_wJqBMAFX4I1H6qhqT04cjStP"
-    },
-    {
-      id: 6,
-      nombre: "Anteojos de sol",
-      precio: 1500,
-      cantidad: 10,
-      img: "https://p.vitalmtb.com/photos/features/2050/title_image/s1600_Leatt2018_BikeDBX_8901_ChrisLaue_656882.jpg?VersionId=_lWWPlR_wJqBMAFX4I1H6qhqT04cjStP"
-    },
-  ];
 
   const [productos, setProductos] = useState([]);
 
